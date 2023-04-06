dae8f83de9c2330cf5e98db0d2f33fd8ba9303e8
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-04-06 02:44:27 +0100

    feat:added and modified Catalog

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Catalog.Entities;
+
+using Catalog.Repositories;
+
 namespace Catalog.Controllers
 {
-	public class ItemsController
+	[ApiController]
+	[Route("items")]
+	public class ItemsController : ControllerBase
 	{
+		private readonly InMemItemsRepository repository;
+
 		public ItemsController()
 		{
+			repository = new InMemItemsRepository();
+		}
+		//Get /items
+		[HttpGet]
+		public IEnumerable<Item> GetItems()
+		{
+			var items = repository.GetItems();
+			return items;
 		}
 	}
 }
