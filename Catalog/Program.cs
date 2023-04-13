9fab832721a1a22876caa375b6dd601a88d688cd
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-04-13 09:50:08 +0100

    Update:added mongoDb configutations to program.cs , modified settings for MongoDbSettings.cs

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,14 +1,31 @@
 using Catalog.Repositories;
 
+using MongoDB.Driver;
+
+using MongoDB.Bson.Serialization;
+
+using MongoDB.Bson.Serialization.Serializers;
+
+using MongoDB.Bson;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddScoped<IInMemItemsRepository, InMemItemsRepository>();
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+
+builder.Services.Configure<MongoDatabaseSettings>(builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<IInMemItemsRepository, InMemItemsRepository>();
 
 builder.Services.AddSwaggerGen();
 
