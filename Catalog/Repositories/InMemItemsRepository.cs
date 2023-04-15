8d01a9f87a1dffdec5768565d9e700e26119f2e0
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-04-15 23:26:21 +0100

    Update:updated methods to async methods

--- Catalog/Repositories/InMemItemsRepository.cs
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -4,6 +4,8 @@ using  Catalog.Entities;
 
 using System.Linq;
 
+//we don't really need the repositories class again but for learning purposes ,we will convert all the methods
+// into async methods
 namespace Catalog.Repositories
 {
     public class InMemItemsRepository : IInMemItemsRepository
@@ -15,29 +17,38 @@ namespace Catalog.Repositories
             new Item {Id = Guid.NewGuid(), Name = "potion",Price = 9,CreatedDate = DateTimeOffset.UtcNow}
         };
 
-        public IEnumerable<Item> GetItemsAsync()
+        public async Task<IEnumerable<Item>> GetItemsAsync()
         {
-            return items;
+            // we will await it nomrally, but since we don't have any asynchrnous method
+            //to call , we will use Task.fromResult - this means we want to create a task that already completed
+            // and we want to intriduc the value of the items into that Task
+            return await Task.FromResult(items);
         }
-        public Item GetItemAsync(Guid id)
+
+        public async Task<Item> GetItemAsync(Guid id)
         {
-            return items.Where(item => item.Id == id).SingleOrDefault();
+
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+            return await Task.FromResult(item);
         }
-        public void CreateItemAsync(Item item)
+        public async Task CreateItemAsync(Item item)
         {
             items.Add(item);
+            await Task.CompletedTask;
         }
-        public void UpdateItemAsync(Item item)
+        public async Task UpdateItemAsync(Item item)
         {
             // we find theindex of the item we are looking for , then update it 
             var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
             // then we update the item we reassigning the item at that index 
             items[index] = item;
+            await Task.CompletedTask;
         }
-        public void DeleteItemAsync(Guid id)
+        public async Task DeleteItemAsync(Guid id)
         {
             var index = items.FindIndex(existingItem => existingItem.Id == id);
             items.RemoveAt(index);
+            await Task.CompletedTask;
         }
     }
 }
--- Catalog/Repositories/MongoDbItemsRepository.cs
+++ Catalog/Repositories/MongoDbItemsRepository.cs
@@ -24,35 +24,38 @@ namespace Catalog.Repositories
             IMongoDatabase database = mongoClient.GetDatabase(databaseName);
             itemsCollection = database.GetCollection<Item>(collectionName);
         }
-        public void CreateItemAsync(Item item)
+        public async Task CreateItemAsync(Item item)
         {
-            itemsCollection.InsertOne(item);
+           await itemsCollection.InsertOneAsync(item);
         }
 
-        public void DeleteItemAsync(Guid id)
-        {
-            throw new NotImplementedException();
-
-        }
-
-        public Item GetItemAsync(Guid id)
+        public async Task DeleteItemAsync(Guid id)
         {
             var filter = filterBuilder.Eq(item => item.Id, id);
-            return itemsCollection.Find(filter).SingleOrDefault();
+            await itemsCollection.DeleteOneAsync(filter);
         }
 
-        public IEnumerable<Item> GetItemsAsync()
+        public async Task<Item> GetItemAsync(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            return await itemsCollection.Find(filter).SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync() 
         {
             // Gives us the list of items in the collection 
-            return itemsCollection.Find(new BsonDocument()).ToList();
+            return await itemsCollection.Find(new BsonDocument()).ToListAsync();
         }
-
-        public void UpdateItemAsync(Item item)
+         
+        public async Task UpdateItemAsync(Item item)
         {
             // we need to use a filter here too, to tell which item to update
             var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
-            itemsCollection.ReplaceOne(filter, item);
+            // we aren't returning anything here
+            await itemsCollection.ReplaceOneAsync(filter, item);
         }
     }
+
+
 }
 
