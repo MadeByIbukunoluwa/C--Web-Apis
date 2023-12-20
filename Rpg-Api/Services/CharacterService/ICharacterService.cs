70d78550debb8432ec50aeaeec74dd3cc6d1b80e
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-12-20 10:59:27 +0100

    feat: added UpdateCharacter to Dto

--- Rpg-Api/Services/CharacterService/ICharacterService.cs
+++ Rpg-Api/Services/CharacterService/ICharacterService.cs
@@ -12,6 +12,7 @@ namespace Rpg_Api.Services.CharacterService
 		Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
 
         Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
+        Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
 	}
 }
 
